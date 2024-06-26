62277766f26e5c6aa7fd3eb3dbca7f789465a6b5
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-26 17:53:11 +0000

    changed the number of contestants to variables

--- main.cs
+++ main.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Linq;
-using System.Collections;
-using System.Text;
+
 
 class Program
 {
@@ -18,12 +16,17 @@ class Program
          Console.WriteLine("1.  CALCULATE Greenville Revenue Year-Over-Year");
          Console.WriteLine("2.  Exit");
    
-         int input = Convert.ToInt32(Console.ReadLine());
+        int input = Convert.ToInt32(Console.ReadLine());
+      
           if (input == 1)
             {
-              Console.WriteLine("\nLast years competition has 122 contestants, and this year's has 426 contestants");
+              Console.WriteLine("Input number of contestants at last year's competition");
+              int lastYear = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Input number of contestants at this year's competition");
+              int thisYear = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("\nLast years competition had " + lastYear +" contestants, and this year's has " + thisYear + " contestants");
               Console.WriteLine("Revenue expected this year is $10,650");
-              Console.WriteLine("It is True that this year's competition is bigger than last years\n");
+              Console.WriteLine("It is " + (thisYear > lastYear) + " that this year's competition is bigger than last years\n");
               continue;
             }
           else if (input == 2)
